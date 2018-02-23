beeacd97b0938100987c9f5eb13912694cda44fe
Nawfal Al Hashimy <nawfal@ECS-Mac-Mar-4.local>
2018-02-23 10:38:32 +0000

    emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -1,28 +1,34 @@
-const http = require('http');
 const path = require('path');
+const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-//const hbs = require('hbs');
-const publicPath = path.join(__dirname,'../public');
+
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3012;
-
-
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', (socket)=>{
+io.on('connection', (socket) => {
   console.log('New user connected');
 
 
-socket.on('disconnect', ()=>{
-  console.log('User was diconnected');
+  socket.on('createMessage', (message) => {
+    console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
+  });
+
+  socket.on('disconnect', () => {
+    console.log('User was disconnected');
+  });
 });
 
-});
-
-server.listen(port,()=>{
-  console.log(`server is up on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server is up on ${port}`);
 });
