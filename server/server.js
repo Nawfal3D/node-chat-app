2c1ccc8c4708b6be8e2ed8048a37bc07ec79aa2d
Nawfal Al Hashimy <nawfal@ECS-Mac-Mar-4.local>
2018-02-23 18:23:02 +0000

    add form for messages and show messages in browser

--- server/server.js
+++ server/server.js
@@ -2,26 +2,28 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-
+const {
+  generateMessage
+} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
-const port = process.env.PORT || 3012;
+const port = process.env.PORT || 3010;
 var app = express();
 var server = http.createServer(app);
 var io = socketIO(server);
-
+var callbackMessage = 'got it again';
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  socket.emit('newMessage', generateMessage('admin', 'Welcome to the chat app'));
 
-  socket.on('createMessage', (message) => {
+  socket.broadcast.emit('newMessage', generateMessage('admin', 'New user joined'));
+
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
+    callback(callbackMessage);
   });
 
   socket.on('disconnect', () => {
